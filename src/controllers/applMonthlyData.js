8bef9f137ae84d7b8acdc0bf915bafd7901ab8ca
Christopher Peterson <crpete23@gmail.com>
2019-01-31 09:10:44 -0800

    modifying api response to fit coding challenge format

--- src/controllers/applMonthlyData.js
+++ src/controllers/applMonthlyData.js
@@ -1,11 +1,15 @@
 const model = require('../models/applMonthlyData')
-const resourceName = 'data'
+const resourceName = 'initialDataSet'
 
 async function getAll(req, res, next){
   try{
     const response = await model.getAll()
+    var modified = response.data.map((object)=>{
+      return ([object.date, object.close])
+    })
     res.status(200).json({
-      [resourceName]: response.data
+      format: 'date',
+      [resourceName]: modified
     })
   } catch (e){
     console.log(e)
